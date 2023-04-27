102a3c2cffbcc65d1c4a32337db23a83e3af2ca1
Brian Bissette <bbissette3@yahoo.com>
2023-04-27 15:37:58 -0500

    done stuff

---
+++ server/migrations/versions/5627baff97d1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5627baff97d1
+Revises: c93a14d1aa19
+Create Date: 2023-04-27 15:25:37.255498
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5627baff97d1'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
